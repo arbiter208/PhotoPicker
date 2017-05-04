be511c94963070842b6b80bc061f2fe470a6aa34
huangshuisheng <shuisheng.huang@qxinli.com>
2017-05-04 15:17:45 +0800

    完美切换

--- PhotoPicker-glide/src/main/java/me/iwf/photopicker/utils/Util2.java
+++
@@ -1,8 +0,0 @@
-package me.iwf.photopicker.utils;
-
-/**
- * Created by Administrator on 2017/5/2.
- */
-
-public class Util2 {
-}
--- photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/BaseApp.java
+++ photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/BaseApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.os.Build;
 import android.os.Bundle;
 
+import com.hss01248.frescopicker.FrescoIniter;
 import com.squareup.leakcanary.LeakCanary;
 
 import me.iwf.photopicker.PhotoPickUtils;
@@ -20,7 +21,7 @@ public class BaseApp extends Application {
     public void onCreate() {
         super.onCreate();
         //ImageLoader.init(this,60,new GlideLoader());
-        PhotoPickUtils.init(this);
+        PhotoPickUtils.init(this,new FrescoIniter());
         LeakCanary.install(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
